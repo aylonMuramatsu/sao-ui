9934895688da15040b0f17ba4cafe833bc77e082
Aylon Muramatsu <aylon.muramatsu@nextsi.com.br>
2017-05-24 13:12:04 -0300

    Criado a base do modal

--- sao-ui/src/App.js
+++ sao-ui/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Login from './views/login';
+// import Login from './views/login';
+import Model from './components/modal/model';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Login/>
+        <Model 
+          title="Alerta!"
+          message="Hello World"
+        />
       </div>
     );
   }
--- sao-ui/src/components/modal/model.jsx
+++ sao-ui/src/components/modal/model.jsx
@@ -1,1 +1,22 @@
 import React from 'react'
+const btn_ok = require('../../assets/SAO_Icons_v3.300/4_Buttons/Yes.png');
+const btn_cancel = require('../../assets/SAO_Icons_v3.300/4_Buttons/No.png');
+export default props => (
+    <div className="modal">
+        <div className="modal-header">
+            {props.title}
+        </div>
+        <div className="modal-body">
+            {props.message}
+        </div>
+        <div className="modal-footer">
+            <div className="btn btn-ok">
+                <img src={btn_ok} alt="" height="45px"/>
+            </div>
+            <div className="btn btn-cancel">
+                <img src={btn_cancel} alt="" height="45px"/>
+            </div>
+            <div className="clearfix"></div>
+        </div>
+    </div>
+)
